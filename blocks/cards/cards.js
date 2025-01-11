0e755d5040edad5f8cf5356de0b8a12f7dd2a698
Adrien <adrien.cappelaere@gmail.com>
2025-01-11 22:22:30 +0100

    Add style, JS effect on cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -15,4 +15,17 @@ export default function decorate(block) {
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
+  // Add hover effect to card list items
+  const cardLists = block.querySelectorAll('ul li');
+  cardLists.forEach(item => {
+    item.addEventListener('mouseover', function() {
+      this.style.transform = 'scale(1.05)';
+      this.style.transition = 'transform 0.3s ease';
+    });
+    
+    item.addEventListener('mouseout', function() {
+      this.style.transform = 'scale(1)';
+    });
+  });
 }
